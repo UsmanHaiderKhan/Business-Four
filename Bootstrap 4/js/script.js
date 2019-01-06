94a5cce5d048b756208ac3046639ee28ae4887d3
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-01-06 17:56:36 +0500

    first commit

---
+++ Bootstrap 4/js/script.js
@@ -0,0 +1,268 @@
+﻿console.log("usman");
+
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav2 = $(".navbar-brand");
+        $nav2.toggleClass('scrolled', $(this).scrollTop() > $nav2.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav3 = $(".navbar-nav");
+        $nav3.toggleClass('scrolled', $(this).scrollTop() > $nav3.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav4 = $(".navbar-light .navbar-nav .nav-link");
+        $nav4.toggleClass('scrolled', $(this).scrollTop() > $nav4.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav4 = $(".logo1");
+        $nav4.toggleClass('scrolled', $(this).scrollTop() > $nav4.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav4 = $(".navbar-toggler");
+        $nav4.toggleClass('scrolled', $(this).scrollTop() > $nav4.height());
+    });
+});
+
+
+
+/*================== Read More Text ==================*/
+$(function () {
+    var showChar = 200;
+    var moretext = `Learn more &nbsp; <i class="fas fa-arrow-right"></i>`;
+    var lesstext = `Learn less &nbsp; <i class="fas fa-arrow-left"></i>`;
+    $('.comments-space').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-content"><span>' + hide_content +
+                '</span><a href="" class="morelinks btn btn-read mt-41" style="display:block">' + moretext + '</a>' +
+                '</span>';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelinks").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+$(function () {
+    var showChar = 180;
+    var moretext = `Read more &nbsp; <i class='fa fa-arrow-right'></i>`;
+    var lesstext = `Read less &nbsp; <i class='fa fa-arrow-left'></i>`;
+    $('.comments-spaces').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-content"><span>' + hide_content +
+                '</span><a href="" class="morelink btn btn-read arrow-outline link-style  mt-155" style="display:block">' + moretext + '</a>' +
+                '</span> ' + '';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelink").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+//Custom
+
+/*===================== Load More Images ======================*/
+$(document).ready(function () {
+
+    $('.loadMore').loadMoreResults({
+        displayedItems: 3,
+        showItems: 3
+
+    });
+
+});
+$(function () {
+    $('a').smoothScroll();
+});
+/*======================= Owl Carousel =======================*/
+$(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: false,
+        navigation: true,
+        navigationText: ["", ""],
+        slideSpeed: 1000,
+        autoPlay: true
+    });
+});
+/*===================== Js Ranks Counter ====================*/
+$(document).ready(function () {
+
+    var counters = $(".counter-value");
+    var countersQuantity = counters.length;
+    var counter = [];
+
+    for (i = 0; i < countersQuantity; i++) {
+        counter[i] = parseInt(counters[i].innerHTML);
+    }
+
+    var count = function (start, value, id) {
+        var localStart = start;
+        setInterval(function () {
+            if (localStart < value) {
+                localStart++;
+                counters[id].innerHTML = localStart;
+            }
+        }, 2);
+    }
+
+    for (j = 0; j < countersQuantity; j++) {
+        count(0, counter[j], j);
+    }
+});
+/*=================== Change Bannner on Click on Banner =================== */
+function change_banner(id) {
+    $('.hello').addClass('d-none');
+    $('#img-' + id).removeClass('d-none');
+    $('.yes-sir').addClass('chah-overlay');
+    $('#overlay-' + id).removeClass('chah-overlay');
+
+
+}
+
+function change_overlay(id) {
+    debugger
+}
+/*==================== 3D Slider Java Script ====================*/
+jQuery(document).ready(function () {
+
+    function detect_active() {
+        // get active
+        var get_active = $("#dp-slider .dp_item:first-child").data("class");
+        $("#dp-dots li").removeClass("active");
+        $("#dp-dots li[data-class=" + get_active + "]").addClass("active");
+    }
+    $("#dp-next").click(function () {
+        var total = $(".dp_item").length;
+        $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+        detect_active();
+
+    });
+
+    $("#dp-prev").click(function () {
+        var total = $(".dp_item").length;
+        $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+
+        detect_active();
+    });
+
+    $("#dp-dots li").click(function () {
+        $("#dp-dots li").removeClass("active");
+        $(this).addClass("active");
+        var get_slide = $(this).attr('data-class');
+        console.log(get_slide);
+        $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+    });
+
+
+    $("body").on("click", "#dp-slider .dp_item:not(:first-child)", function () {
+        var get_slide = $(this).attr('data-class');
+        console.log(get_slide);
+        $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+
+        detect_active();
+    });
+});
+
+/*=================== Filterizer Js ====================*/
+
+$(document).ready(function () {
+    // var filterizd = $(".filtr-container").filterizr({})
+    $(".btn-act").on('click', function () {
+        $(".btn-act").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+filterSelection("all")
+
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
